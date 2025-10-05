147a77c1d381b71a419989388beee94d94bc9317
AndrePes <mail@andre-peschyras.de>
2025-10-05 08:12:18 +0200

    Some changes

--- src/main.js
+++ src/main.js
@@ -8,7 +8,10 @@ const app = express()
 const port = 1337
 
 // Load local directory from environment variable or use default
-const localDir = process.env.LOCAL_DIR || './src/raw';
+const localDir = process.env.LOCAL_DIR || './src/data';
+console.log(`Using directory: ${localDir}`);
+
+
 
 // Array to hold parsed data
 var data = [];
@@ -23,33 +26,79 @@ app.use((req, res, next) => {
   next();
 });
 
+function getDirectories() {
+    console.log(`Reading directories from: ${localDir}`);
+    return fs.readdirSync(localDir)
+        .map(file => path.join(localDir, file))
+        .filter(path => fs.statSync(path).isDirectory());
+}
+
+console.log(`Found directories: ${getDirectories().map(dir => path.basename(dir)).join(', ')}`);
+
 /*
 * Reads all JSON files from the local directory and parses them into an array of objects.
 */
 const readFiles = () => {
-    const data = [];
-    const aFiles = fs.readdirSync(localDir).filter(file => path.extname(file) === '.json');
-    console.log(`Found ${aFiles.length} files.`);
+    const aData = [];
+    const aDirectories = getDirectories();
 
-    //Load each file content to memory
-    aFiles.forEach(file => {
-        const content = fs.readFileSync(path.join(localDir, file));
-        console.log(`Loading file: ${file}`);
-        const json = JSON.parse(content.toString());
+    // Iterate through each directory
+    aDirectories.forEach(dir => {
+      console.log(`Reading directory: ${dir}`);
+      
+      //Read all files in the directory and filter for .json files
+      const sPath = path.join(dir, 'Reports', 'raw');
 
-        // TODO: Erstellle ein Array für jede MemberId und füge alle Shots hinzu
-        // TODO: Finde die richten teiler im Shots Array.
+      if (!fs.existsSync(sPath)) {
+        console.warn(`Directory does not exist: ${sPath}`);
+        return;
+      }
 
-        data.push({ 
-            id: json.UserSessionInformation.UserData.MemberId || "undefined",
-            firstName: json.UserSessionInformation.UserData.FirstName || "undefined",
-            lastName: json.UserSessionInformation.UserData.Name || "undefined",
-            date: String(json.ExerciseStart).split('T')[0] || "undefined",
-            splits: json.ParameterResults[0].Teilers || []
-        });
+      const aFiles = fs.readdirSync(sPath).filter(file => path.extname(file) === '.json');
+
+      console.log(`Found ${aFiles.length} files.`);
+
+      //Load each file content to memory
+      aFiles.forEach(file => {
+          const content = fs.readFileSync(path.join(sPath, file));
+          console.log(`Loading file: ${file}`);
+          const json = JSON.parse(content.toString());
+
+          aData.push({
+            id: json?.UserSessionInformation?.UserData?.MemberId || 'unknown',
+            firstName: json?.UserSessionInformation?.UserData?.FirstName || 'unknown',
+            lastName: json?.UserSessionInformation?.UserData?.Name || 'unknown',
+            date: path.basename(dir) || 'unknown',
+            splits: json?.ParameterResults[0]?.Teilers || [0.0, 0.0, 0.0]
+          });
+      });
     });
-    return data;    
-  }
+
+    return aData;    
+}
+
+readFiles();
+
+
+const groupById = (rawData) => {
+    return rawData.reduce((acc, result) => {
+        const memberId = result?.id;
+        if (!memberId) return acc;
+        if (!acc[memberId]) acc[memberId] = [];
+        acc[memberId].push(result);
+        return acc;
+    }, {});
+}
+
+const groupByDate = (rawData) => {
+    return rawData.reduce((acc, result) => {
+        const date = result?.date;
+        if (!date) return acc;
+        if (!acc[date]) acc[date] = [];
+        acc[date].push(result);
+        return acc;
+    }, {});
+}
 
 /*
 * Default route to check if the server is running.
@@ -61,8 +110,16 @@ app.get('/', (req, res) => {
 /*
 * API endpoint to get the parsed data from JSON files.
 */
-app.get('/api/read', (req, res) => {
+app.get('/api/raw', (req, res) => {
   res.send(readFiles())
+})
+
+app.get('/api/groupById', (req, res) => {
+  res.send(groupById(readFiles()))
+})
+
+app.get('/api/groupByDate', (req, res) => {
+  res.send(groupByDate(readFiles()))
 })
 
 /*
